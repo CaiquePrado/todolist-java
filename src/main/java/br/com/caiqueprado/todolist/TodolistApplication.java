383e849f1fcdc0bf39b1661116129b7e40b41a7f
CaiquePrado <caiqueprado76@gmail.com>
2023-10-14 16:01:12 -0300

    chore: todolist

---
+++ src/main/java/br/com/caiqueprado/todolist/TodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.caiqueprado.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
